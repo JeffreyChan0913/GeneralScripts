163a44ff03fabdf238661c922bd44e05510a7fff
jeffrey <jeffrey913@outlook.com>
2023-09-02 23:58:14 -0700

    mod scpt

--- startCompetitiveProgrammingTeamplate.cpp
+++ startCompetitiveProgrammingTeamplate.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(int argc, char *argv[]){
-    string template_name = (atoi(argv[2]) == 1) ? "template.cpp ./" : "template_base.cpp ./";
+    string template_name = (atoi(argv[2]) == 1) ? "template.cpp ./" : "template_cses.cpp ./";
     string command = "cp /Users/jeffreychan/Documents/script/" + template_name;
     string filename(argv[1]);
     command += filename + ".cpp && vim " + filename + ".cpp";
